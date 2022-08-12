fcc0af59f80892481488168373c8575bcd0bfff6
Sara Nurestani <saranurestani@gmail.com>
2022-08-12 14:00:08 +0100

    Use TryConvert

--- app/index.test.js
+++ app/index.test.js
@@ -1,20 +1,21 @@
 
+import tryConvert from "./index.js";
 
 test("convert from 100 Celsius toFahrenheit", () => {
-  expect(input(100)).toBe(212);
+  expect(tryConvert(100)).toBe(212);
 });
 
 test("convert from 50 Fahrenheit toCelsius", () => {
-  expect(input(50)).toBe(10);
+  expect(tryConvert(50)).toBe(10);
 });
 
 test("convert to Celsius with non-numerical input", () => {
-  expect(input(NaN)).toBe("blank string"):
+  expect(tryConvert(NaN)).toBe("blank string"):
 }
 )
 
 test("convert to Fahrenheit with non-numerical input", () => {
-  expect(input(NaN)).toBe("blank string");
+  expect(tryConvert(NaN)).toBe("blank string");
 }
 )
 
