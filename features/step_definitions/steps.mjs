b1ef2768a9e57fb028ab1764b02485aa717d65e4
Sara Nurestani <saranurestani@gmail.com>
2022-08-18 13:30:58 +0100

    Convert temperature

--- features/step_definitions/steps.mjs
+++ features/step_definitions/steps.mjs
@@ -3,17 +3,26 @@ import { expect } from "expect";
 import tryConvert from "../../app/index.js";
 
 Given("I have a temperature of {float}° {units}", function (temp2Convert, _) {
+  console.log(this, "this is the this");
   this.temp2Convert = temp2Convert;
 });
 
+Given("I have non-numerical input", () => {});
+
 When("I convert it to degrees {units}", function (units) {
+  console.log(this, "this is the this");
   this.convertedTemp = tryConvert(this.temp2Convert, units);
 });
 
 Then("I see {float}° {units}", function (expected, _) {
   const expectHelper = () => {
+    console.log(this, "this is the this");
     expect(this.convertedTemp).toBe(expected.toString());
   };
+});
 
-  expectHelper();
+Then('I "see" a blank string', function () {
+  const expectHelper = () => {
+    expect(this.convertedTemp).toBe("");
+  };
 });
--- features/support/units_parameters.mjs
+++ features/support/units_parameters.mjs
@@ -2,7 +2,7 @@ import { defineParameterType } from "@cucumber/cucumber";
 
 defineParameterType({
   name: "units",
-  regexp: /C|F|Celsius|Fahrenheit/,
+  regexp: /\s?C|\s?F|\s?Celsius|\s?Fahrenheit/,
   transformer(name) {
     return name.startsWith("C") ? "toCelsius" : "toFahrenheit";
   },
